7569ac73e2b9d036c9390e48f9dfe5764bacefef
justinveiner <justin.veiner@github.com>
2017-11-30 21:16:00 -0500

    ReportModal
    Created reportModal.js (not yet complete)

--- client/js/reportModal.js
+++ client/js/reportModal.js
@@ -1,4 +1,35 @@
-var DBRow = require('./../DBRow').DBRow;
-var lit = require('./../Literals');
-var log = require('./../log');
+"use strict"
 
+function submitItem() {
+    newContent = {
+        requested: "reportModal",
+        type: getPressed().replace('#', ''),
+        content: getContent(getPressed().replace('#', ''))
+    };
+
+    if (!checkFields(newContent.type))
+        return;
+
+//info --> requestResponder --> build handler--> add case in requestResponder
+
+    var buttonName = '#' + newContent.type + '-button';
+
+    $(buttonName).prop('disabled', true);
+
+    var href = 'report';
+
+    AJAXCall(href, newContent, false, onSuccessfulInsert);
+
+}
+
+function onSuccessfulInsert(data) {
+    if (data.id)
+        document.location = '/' + newContent.type + '?id=' + data.id;
+    else
+        console.error('Error inserting new item');
+}
+
+function addReport()
+{
+
+}
