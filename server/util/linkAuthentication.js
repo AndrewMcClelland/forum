f144a783c795b6845e8a1715035d761412589db6
KrishnaIyer99 <krishnaiyer28@gmail.com>
2018-02-22 16:00:14 -0500

    creating the feedback handler

---
+++ server/util/linkAuthentication.js
@@ -0,0 +1,33 @@
+/*
+** linkAuthentication.js
+** Created by Krishna Iyer 12/23/17
+*
+** Aunthenticates links posted by users on the forum
+*/
+
+"use strict";
+function linkAunthenticator(url) {
+    return new Promise(function(resolve,reject){
+            //get the required modules
+            var SafeBrowse = require('node-safe-browse');
+            var api = new SafeBrowse.Api(/*Put API key here*/);
+
+            //Check if link is safe
+            api.lookup(url)
+                .on('success', function (data) {
+                    resolve(url);
+                })
+                .on('error', function (error) {
+                    var warning = new Error('This link is not safe!');
+                    reject(warning);
+                });
+        });
+}
+
+
+
+
+
+
+
+
